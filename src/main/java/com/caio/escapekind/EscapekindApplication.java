7be8a97193462d84d78d8be42cb63bcc05e930d9
Caio Almeida <caiofealmeida@gmail.com>
2026-04-17 02:01:33 +0200

    Projeto Spring Boot inicial criado com Maven

---
+++ src/main/java/com/caio/escapekind/EscapekindApplication.java
@@ -0,0 +1,13 @@
+package com.caio.escapekind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapekindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscapekindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caio/escapekind/EscapekindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.escapekind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapekindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
